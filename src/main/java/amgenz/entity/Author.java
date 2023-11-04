9f629d391175b507cb7ecc16316554f325786d9f
Abigail Genz <amgenz@madisoncollege.edu>
2023-11-03 19:03:46 -0500

    created book, author and genre entities

--- src/main/java/amgenz/entity/Author.java
+++ src/main/java/amgenz/entity/Author.java
@@ -1,4 +1,163 @@
 package amgenz.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * The type Author.
+ */
+@Entity(name = "Author")
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "middle_Name")
+    private String middleName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String about;
+
+    /**
+     * Instantiates a new Author.
+     */
+    public Author() {
+    }
+
+    /**
+     * Instantiates a new Recipe.
+     *
+     * @param firstName  author's first name
+     * @param middleName author's middle name
+     * @param lastName   author's last name
+     * @param about      author's about information
+     */
+    public Author(String firstName, String middleName, String lastName, String about) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.about = about;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets middle name.
+     *
+     * @return the middle name
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    /**
+     * Sets middle name.
+     *
+     * @param middleName the middle name
+     */
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets about.
+     *
+     * @return the about
+     */
+    public String getAbout() {
+        return about;
+    }
+
+    /**
+     * Sets about.
+     *
+     * @param about the about
+     */
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", about='" + about + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id && firstName.equals(author.firstName) && middleName.equals(author.middleName) && lastName.equals(author.lastName) && about.equals(author.about);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, middleName, lastName, about);
+    }
 }
+
+
+
--- src/main/java/amgenz/entity/Book.java
+++ src/main/java/amgenz/entity/Book.java
@@ -1,4 +1,135 @@
 package amgenz.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * The type Book.
+ */
+@Entity(name = "Book")
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    private String title;
+    private String summary;
+    private int series;
+    @ManyToOne
+    private Author author;
+    @ManyToOne
+    private Genre genre;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Author> authorList = new HashSet<>();
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Genre> genreList = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String summary, int series, Author author, Genre genre) {
+        this.title = title;
+        this.summary = summary;
+        this.series = series;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Set<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(Set<Author> authorList) {
+        this.authorList = authorList;
+    }
+
+    public Set<Genre> getGenreList() {
+        return genreList;
+    }
+
+    public void setGenreList(Set<Genre> genreList) {
+        this.genreList = genreList;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", summary='" + summary + '\'' +
+                ", series=" + series +
+                ", author=" + author +
+                ", genre=" + genre +
+                ", authorList=" + authorList +
+                ", genreList=" + genreList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && series == book.series && title.equals(book.title) && Objects.equals(summary, book.summary) && author.equals(book.author) && genre.equals(book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, summary, series, author, genre, authorList, genreList);
+    }
 }
